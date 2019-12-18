319ffab655aefb0089a82bcf0df519cf2c7e9cb3
Naveh Mevorach <navehmevorach@gmail.com>
2019-12-19 00:18:35 +0200

    style changes

--- instads-app/src/App.js
+++ instads-app/src/App.js
@@ -1,60 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FeaturedUsers from "./components/featuredUsers";
-import UserProfiles from "./components/userProfiles";
-import Api from './api/api';
-import NavCategories from './components/navCategories';
-import Login from './components/login';
-import Signup from './components/signup';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import FeaturedUsers from './components/featuredUsers'
+import UserProfiles from './components/userProfiles'
+import Api from './api/api'
+import NavCategories from './components/navCategories'
+import Login from './components/login'
+import Signup from './components/signup'
 
 class App extends React.Component {
-  _api;
+  _api
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       filter: null,
       users: null,
       error: null,
       isLoginModalOpen: false,
       isSignupModalOpen: false
-    };
-    this._api = new Api();
-    this.handleFilterChange = this.handleFilterChange.bind(this);
-    this.openLoginModal = this.openLoginModal.bind(this);
-    this.closeLoginModal = this.closeLoginModal.bind(this);
-    this.openSignupModal = this.openSignupModal.bind(this);
-    this.closeSignupModal = this.closeSignupModal.bind(this);
-    this.login = this.login.bind(this);
-    this.signup = this.signup.bind(this);
+    }
+    this._api = new Api()
+    this.handleFilterChange = this.handleFilterChange.bind(this)
+    this.openLoginModal = this.openLoginModal.bind(this)
+    this.closeLoginModal = this.closeLoginModal.bind(this)
+    this.openSignupModal = this.openSignupModal.bind(this)
+    this.closeSignupModal = this.closeSignupModal.bind(this)
+    this.login = this.login.bind(this)
+    this.signup = this.signup.bind(this)
   }
   componentDidMount() {
-    this._api.getUsers()
-      .then(users => { this.setState({ users: users, error: null }); })
-      .catch(error => { this.setState({ error: error }); });
+    this._api
+      .getUsers()
+      .then(users => {
+        this.setState({ users: users, error: null })
+      })
+      .catch(error => {
+        this.setState({ error: error })
+      })
   }
   handleFilterChange(event) {
-    this.setState({ filter: event.target.value });
+    this.setState({ filter: event.target.value })
   }
   openLoginModal() {
-    this.setState({ isLoginModalOpen: true, isSignupModalOpen: false });
+    this.setState({ isLoginModalOpen: true, isSignupModalOpen: false })
   }
   closeLoginModal() {
-    this.setState({ isLoginModalOpen: false });
+    this.setState({ isLoginModalOpen: false })
   }
   openSignupModal() {
-    this.setState({ isSignupModalOpen: true, isLoginModalOpen: false });
+    this.setState({ isSignupModalOpen: true, isLoginModalOpen: false })
   }
   closeSignupModal() {
-    this.setState({ isSignupModalOpen: false });
+    this.setState({ isSignupModalOpen: false })
   }
   login(authData) {
-    this._api.login(authData)
-      .then(() => this.closeLoginModal());
+    this._api.login(authData).then(() => this.closeLoginModal())
   }
   signup(signupData) {
-    this._api.signup(signupData)
-      .then(() => this.closeSignupModal());
+    this._api.signup(signupData).then(() => this.closeSignupModal())
   }
   render() {
     if (!this.state.users && !this.state.error) {
@@ -63,107 +66,150 @@ class App extends React.Component {
     if (this.state.error && !this.state.users) {
       return <p>${this.state.error.message}</p>
     }
-    let users = this.state.users;
+    let users = this.state.users
     if (this.state.filter) {
-      users = users.filter(user => user.name.includes(this.state.filter));
+      users = users.filter(user => user.name.includes(this.state.filter))
     }
     return (
       <body>
-        <div className="container">
-          <nav className="nav">
-            <div className="nav__row">
-              <div className="logo-box">
-                <a className="logo" href="">INSTADS</a>
+        <div className='container'>
+          <nav className='nav'>
+            <div className='nav__row'>
+              <div className='logo-box'>
+                <a className='logo' href=''>
+                  INSTADS
+                </a>
               </div>
-              <div className="nav__list">
-                <a href="">Contact</a>
-                <a href="">Help</a>
+              <div className='nav__list'>
+                <a href=''>Contact</a>
+                <a href=''>Help</a>
                 <a onClick={this.openLoginModal}>Sign in</a>
-                <button onClick={this.openSignupModal} className="join trigger">Become an Influencer</button>
+                <button onClick={this.openSignupModal} className='join trigger'>
+                  Become an Influencer
+                </button>
               </div>
             </div>
             <NavCategories />
           </nav>
-          <header className="header">
-            <div className="header__wrapper">
-              <div className="header__content">
-                <h1 className="header__title">
-                  Find The Perfect Influencer <br />For Your Business
-                    </h1>
-                <label className="header__search-box">
-                  <i className="fas fa-search"></i>
-                  <input type="text" name="" id="" onChange={this.handleFilterChange} className="header__search" placeholder='Try "Photography"' />
+          <header className='header'>
+            <div className='header__wrapper'>
+              <div className='header__content'>
+                <h1 className='header__title'>
+                  Find The Perfect Influencer <br />
+                  For Your Business
+                </h1>
+                <label className='header__search-box'>
+                  <i className='fas fa-search'></i>
+                  <input
+                    type='text'
+                    name=''
+                    id=''
+                    onChange={this.handleFilterChange}
+                    className='header__search'
+                    placeholder='Try "Photography"'
+                  />
                 </label>
+                <button onClick={this.openSignupModal} className='join trigger responsive-signup'>
+                  Become an Influencer
+              </button>
               </div>
-              <div className="header__bg">
-                <img src="./img/bg.svg" alt="background" />
+              <div className='header__bg'>
+                <img src='./img/bg.svg' alt='background' />
               </div>
+
             </div>
           </header>
-          <main className="main">
-            <section className="featured">
+          <main className='main'>
+            <section className='featured'>
               <h3>Featured</h3>
               <FeaturedUsers users={users} />
             </section>
-            <section className="categories">
+            <section className='categories'>
               <h3>Categories</h3>
-              <div className="categories__container">
-                <a href="" className="category">
-                  <i className="fas fa-volleyball-ball"></i>
-                  <p className="category__text">Sports</p>
+              <div className='categories__container'>
+                <a href='' className='category'>
+                  <i className='fas fa-volleyball-ball'></i>
+                  <p className='category__text'>Sports</p>
                 </a>
-                <a href="" className="category">
-                  <i className="fas fa-utensils"></i>
-                  <p className="category__text">Food</p>
+                <a href='' className='category'>
+                  <i className='fas fa-utensils'></i>
+                  <p className='category__text'>Food</p>
                 </a>
-                <a href="" className="category">
-                  <i className="fas fa-tv"></i>
-                  <p className="category__text">TV & Movies</p>
+                <a href='' className='category'>
+                  <i className='fas fa-tv'></i>
+                  <p className='category__text'>TV & Movies</p>
                 </a>
-                <a href="" className="category">
-                  <i className="fas fa-gamepad"></i>
-                  <p className="category__text">Gaming</p>
+                <a href='' className='category'>
+                  <i className='fas fa-gamepad'></i>
+                  <p className='category__text'>Gaming</p>
                 </a>
-                <a href="" className="category">
-                  <i className="fas fa-route"></i>
-                  <p className="category__text">Travel</p>
+                <a href='' className='category'>
+                  <i className='fas fa-route'></i>
+                  <p className='category__text'>Travel</p>
                 </a>
-                <a href="" className="category">
-                  <i className="fas fa-tshirt"></i>
-                  <p className="category__text">Fashion</p>
+                <a href='' className='category'>
+                  <i className='fas fa-tshirt'></i>
+                  <p className='category__text'>Fashion</p>
                 </a>
-                <a href="" className="category">
-                  <i className="fas fa-headphones"></i>
-                  <p className="category__text">Music</p>
+                <a href='' className='category'>
+                  <i className='fas fa-headphones'></i>
+                  <p className='category__text'>Music</p>
                 </a>
-                <a href="" className="category">
-                  <i className="fas fa-music"></i>
-                  <p className="category__text">Dance</p>
+                <a href='' className='category'>
+                  <i className='fas fa-music'></i>
+                  <p className='category__text'>Dance</p>
                 </a>
-                <a href="" className="category">
-                  <i className="fas fa-camera"></i>
-                  <p className="category__text">Photography</p>
+                <a href='' className='category'>
+                  <i className='fas fa-camera'></i>
+                  <p className='category__text'>Photography</p>
                 </a>
               </div>
             </section>
-            <section className="profiles">
-              <p className="profiles__sub-title">All Profiles</p>
+            <section className='profiles'>
+              <p className='profiles__sub-title'>All Profiles</p>
               <h2>Find Your Next Influencer</h2>
               <UserProfiles users={users} />
             </section>
           </main>
+          <li class='footer'>
+            <div class='logo'>INSTADS</div>
+            <ul class='footer__nav'>
+              <li>
+                <a href='#'>About us</a>
+              </li>
+              <li>
+                <a href='#'>Download apps</a>
+              </li>
+              <li>
+                <a href='#' onClick={this.openSignupModal}>Become a Influencer</a>
+              </li>
+              <li>
+                <a href='#'>Careers</a>
+              </li>
+              <li>
+                <a href='#'>Contact</a>
+              </li>
+            </ul>
+            <p class='footer__copyright'>
+              &copy; by INSTADS. All rights reserved.
+            </p>
+          </li>
         </div>
-        <Login isOpen={this.state.isLoginModalOpen}
+        <Login
+          isOpen={this.state.isLoginModalOpen}
           closeModal={this.closeLoginModal}
           signup={this.openSignupModal}
-          login={this.login} />
-        <Signup isOpen={this.state.isSignupModalOpen}
+          login={this.login}
+        />
+        <Signup
+          isOpen={this.state.isSignupModalOpen}
           closeModal={this.closeSignupModal}
           login={this.openLoginModal}
-          signup={this.signup} />
+          signup={this.signup}
+        />
       </body>
-    );
+    )
   }
 }
 
-export default App;
+export default App
