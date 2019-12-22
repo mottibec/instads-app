572595052fc3abaa5e1391d72e2221130e216f05
Naveh Mevorach <navehmevorach@gmail.com>
2019-12-22 16:54:52 +0200

    h

--- instads-app/src/App.js
+++ instads-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import FeaturedUsers from './components/featuredUsers'
 import UserProfiles from './components/userProfiles'
